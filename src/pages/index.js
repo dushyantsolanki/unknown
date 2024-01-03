22df3e845fab0f6717fc3486a42390f42df4e3f6
dushyantsolanki <dushyantsolanki.2002@gmail.com>
2024-01-03 08:50:49 +0530

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import Signup from "./signup/Signup";
+import Login from "./login/Login";
+import PassRecover from "./passwordRecovery/PassRecover";
+import Dashboard from "./dashboard/Dashboard";
+import Profile from "./dashboard/Profile";
+import ErrorPage from "./errorPage/ErrorPage";
+
+export { Signup, Login, PassRecover, Dashboard, ErrorPage, Profile };
